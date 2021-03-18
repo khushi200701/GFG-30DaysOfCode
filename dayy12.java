cdef781771bb244f34192a9a5d876ce328287841
Khushi <58480229+khushi200701@users.noreply.github.com>
2021-03-18 18:22:34 +0530

    Update and rename dayy12 to dayy12.java

--- dayy12.java
+++ dayy12.java
@@ -1,2 +1,3 @@
 //Given an integer array of size  N . You have to find sum of bit differences in all pairs that can be formed from array elements. Bit difference of a pair (x, y) is count of different bits at same positions in binary representations of x and y.
 //For example, bit difference for 2 and 7 is 2. Binary representation of 2 is 010 and 7 is 111 ( first and last bits differ in two numbers).
+
